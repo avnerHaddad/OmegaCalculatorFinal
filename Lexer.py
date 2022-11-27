e789568912e7e190ae445d10e6c623ec7cc28a74
Avner <avner788@gmail.com>
2022-11-28 01:01:37 +0200

    polished the lexer and added documentaion

--- Lexer.py
+++ Lexer.py
@@ -1,39 +1,52 @@
-from Config import TokenDict, digs
+from Config import TokenDict, digs, operators
 from Token import Token
 
 
 class Lexer:
+#lexer class for turning user input into identifiable tokens
     def __init__(self, equation):
         self.equation = equation
         self.iterator = 0
         self.tokens = []
 
     def __insertToken(self, TYPE, value=None):
+        #inserts token to end of the token array
         self.tokens.append(Token(TYPE, value))
 
     def __getTokens(self):
+        #fills up the token array from the user input
         while self.iterator < len(self.equation):
-            if self.__ValAt() in digs:
+            if self.__ValAt() == ' ':
+                self.Next()
+            elif self.__ValAt() in digs:
                 self.__getNumberToken()
-
-
-            else:
+            elif self.__ValAt() in operators:
                 self.__insertToken(TokenDict[self.equation[self.iterator]])
-                self.iterator += 1
+                self.Next()
+            else:
+                raise Exception("character is not a part of the calc")
 
     def __ValAt(self):
+        #return the value/char the iterator is currently at
         if self.iterator < len(self.equation):
             return self.equation[self.iterator]
 
     def __getNumberToken(self):
+        #iterates over rest of the number and creates a number token, called when encountering a digit
+
         number = ''
         while (self.__ValAt() in digs or self.__ValAt() == '.') and self.iterator < len(
                 self.equation):
             number += self.__ValAt()
-            self.iterator += 1
+            self.Next()
 
         self.__insertToken('NUM', float(number))
 
     def GetTokens(self):
+        #func to call externally, return an array of tokens from the input
         self.__getTokens()
         return self.tokens
+
+    def Next(self):
+        #advance the iterator
+        self.iterator+=1
