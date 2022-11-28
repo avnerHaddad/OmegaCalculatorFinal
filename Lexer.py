e9aca6128b4017322baecb0a9c92f056ff7704c8
Avner <avner788@gmail.com>
2022-11-28 19:00:37 +0200

    polished lexer

--- Lexer.py
+++ Lexer.py
@@ -3,50 +3,55 @@ from Token import Token
 
 
 class Lexer:
-#lexer class for turning user input into identifiable tokens
+    # lexer class for turning user input into identifiable tokens
     def __init__(self, equation):
         self.equation = equation
         self.iterator = 0
         self.tokens = []
 
     def __insertToken(self, TYPE, value=None):
-        #inserts token to end of the token array
+        # inserts token to end of the token array
         self.tokens.append(Token(TYPE, value))
 
     def __getTokens(self):
-        #fills up the token array from the user input
+        # fills up the token array from the user input
         while self.iterator < len(self.equation):
-            if self.__ValAt() == ' ':
+            if self.__curChar() == ' ':
                 self.Next()
-            elif self.__ValAt() in digs:
+            elif self.__curChar() in digs:
                 self.__getNumberToken()
-            elif self.__ValAt() in operators:
+            elif self.__curChar() in operators:
                 self.__insertToken(TokenDict[self.equation[self.iterator]])
                 self.Next()
             else:
-                raise Exception("character is not a part of the calc")
+                raise Exception("char" + self.__curChar() + " is not a part of the calc")
 
-    def __ValAt(self):
-        #return the value/char the iterator is currently at
+    def __curChar(self):
+        # return the value/char the iterator is currently at
         if self.iterator < len(self.equation):
             return self.equation[self.iterator]
 
     def __getNumberToken(self):
-        #iterates over rest of the number and creates a number token, called when encountering a digit
+        # iterates over rest of the number and creates a number token, called when encountering a digit
 
         number = ''
-        while (self.__ValAt() in digs or self.__ValAt() == '.') and self.iterator < len(
+        dotCount = 0
+        while (self.__curChar() in digs or self.__curChar() == '.') and self.iterator < len(
                 self.equation):
-            number += self.__ValAt()
+            if(self.__curChar() == '.'):
+                dotCount += 1
+                if(dotCount > 1):
+                    raise Exception("duplicate decimal point")
+            number += self.__curChar()
             self.Next()
 
         self.__insertToken('NUM', float(number))
 
     def GetTokens(self):
-        #func to call externally, return an array of tokens from the input
+        # func to call externally, return an array of tokens from the input
         self.__getTokens()
         return self.tokens
 
     def Next(self):
-        #advance the iterator
-        self.iterator+=1
+        # advance the iterator
+        self.iterator += 1
