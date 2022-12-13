514da3d3c665649bd420e3a8597e58097ba38de3
Avner <avner788@gmail.com>
2022-12-13 12:04:40 +0200

    added parser documentation

--- Mparser.py
+++ Mparser.py
@@ -13,17 +13,19 @@ class Mparser:
         self.fixPostFixOps()
         self.head = self.Parse()
 
+    # func that calls the recursive decent algorithm
     def Parse(self):
         head = self.generciLevel(1)
         return head
 
-
+# updates the value of curVal to what the current index is pointing to
     def __Updatecur(self):
         try:
             self.curVal = self.tokens[self.index]
         except IndexError:
             self.curVal = None
 
+# advances the index one step forward and updates curVal on the way, raises exception if not possible
     def __Next(self):
         try:
             self.index += 1
@@ -31,6 +33,7 @@ class Mparser:
         except IndexError:
             self.curVal = None
 
+    # steps one step backward with index and updates curVal on the way, raises exception if not possible
     def __Back(self):
         try:
             self.index -= 1
@@ -44,6 +47,8 @@ class Mparser:
         else:
             return self.tokens[self.index]
 
+# func that goes over the current tkens and moves back all of the postfix operators to where they should have been
+    # operated lineraly
     def fixPostFixOps(self):
         while self.curVal is not None:
             if self.curVal.type in PostFixOps:
@@ -55,7 +60,7 @@ class Mparser:
                     while self.curVal.type is not 'BRACKET_OPEN':
                         self.__Back()
 
-                while self.index >0 and TokenPowerDict[self.prevVal().type] >= TokenPowerDict[toSwtich.type]:
+                while self.index > 0 and TokenPowerDict[self.prevVal().type] >= TokenPowerDict[toSwtich.type]:
                     self.__Back()
                 self.tokens.insert(self.index, toSwtich)
                 self.NextNonePostFix()
@@ -63,23 +68,25 @@ class Mparser:
         self.index = 0
         self.__Updatecur()
 
-
+# helper function for fixPostFixOps moves the index to the next non post fix object
     def NextNonePostFix(self):
         while self.curVal.type in PostFixOps:
             self.__Next()
 
-    def generciLevel(self,level):
+# the main parsing algorithm, calls itself recursively, returns a head of a miniTree of the specific operators(based
+    # on levels of strength) until it reaches a number and returns a number node
+    def generciLevel(self, level):
         if level == finalLevel:
             return self.finalLevel()
-        head = self.generciLevel(level+1)
+        head = self.generciLevel(level + 1)
 
         while self.curVal is not None and self.curVal.type in levelDict[level] and self.curVal.type not in PostFixOps:
             type = self.curVal.type
             self.__Next()
-            head = TokenNode(head, self.generciLevel(level+1), type)
+            head = TokenNode(head, self.generciLevel(level + 1), type)
         return head
 
-
+# the final part of the previous function, handles the specifics such as SingledigOps, Brackets and Nums
     def finalLevel(self):
         head = self.curVal
         if self.curVal.type == 'BRACKET_OPEN':
@@ -103,10 +110,9 @@ class Mparser:
 
         return head
 
+# returns the value at the index before if possible, like Back but without decreasing index
     def prevVal(self):
         if self.index > 0:
-            return self.tokens[self.index-1]
+            return self.tokens[self.index - 1]
         else:
             raise Exception("error")
-
-
